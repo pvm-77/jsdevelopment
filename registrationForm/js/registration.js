6e6781231ae34478411fb0e7cc7d948e3fb88ff5
pvm-77 <sfzalig@gmail.com>
2021-10-20 18:28:56 +0530

    new registration

---
+++ registrationForm/js/registration.js
@@ -0,0 +1,21 @@
+
+
+
+// handle registration form
+
+
+
+// alert('hi');
+
+let getForm=document.forms[0];
+// console.log(getForm);
+
+getForm.addEventListener('submit',function(){
+
+    getForm.elements[''];
+    getForm.elements[''];
+    getForm.elements[''];
+    getForm.elements[''];
+    getForm.elements[''];
+
+});

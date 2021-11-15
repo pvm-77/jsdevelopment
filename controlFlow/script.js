b59e26a0cd3960d48588b3c6b0d310ee3d8a8ec0
pvm-77 <sfzalig@gmail.com>
2021-11-15 18:03:04 +0530

    new added code

--- controlFlow/script.js
+++ controlFlow/script.js
@@ -1,3 +1,8 @@
+let querySelect = document.querySelector('#query-select');
+let ansSelect = document.querySelector('#answer-select');
+let selectQuantityElement = document.querySelector('#physical_quantity');
+
+
 let numberSystemObject = {
     'bin': 'Binary',
     'ter': 'Ternary',
@@ -49,244 +54,50 @@ let lengthObject = {
     'yd': 'Yard',
     'mi': 'Mile'
 };
-// let lstPhysicalQuantity = [];
-// lstPhysicalQuantity.push(lengthObject, speedObject, massObject, digitalStoreObject, numberSystemObject);
-// console.log(lstPhysicalQuantity);
-// handleChildSelectPhysicalEntity(lstPhysicalQuantity);
 
 
-function handleChildSelectPhysicalEntity(lstPhysicalQuantity, querySelect,answerSelect) {
+
+function handleChildSelectPhysicalEntity(lstPhysicalQuantity, querySelect, answerSelect) {
 
     if (querySelect.hasChildNodes()) {
         querySelect.innerHTML = '';
-            // loop throguh each object in array
-            for (let key in lstPhysicalQuantity) {
-                // console.log(key);
-                // console.log(lstPhysicalQuantity[i][key]);
-                addOption(querySelect, key, lstPhysicalQuantity[key]);
-                addOption(answerSelect, key, lstPhysicalQuantity[key]);
-                // handle child select element value
-            }
-        
+        // loop throguh each object in array
+        for (let key in lstPhysicalQuantity) {
+            // console.log(key);
+
+            addOption(querySelect, key, lstPhysicalQuantity[key]);
+            addOption(answerSelect, key, lstPhysicalQuantity[key]);
+
+        }
+
 
     }
-
-
-
-
-
-
 }
-// let getInput=document.querySelector("input");
 
-// add event listener to the input
-// getInput.addEventListener("blur",function(event){
-
-//     event.preventDefault();
-// get input value
-// var inputValue=document.querySelector('input').value;
-// console.log(inputValue);
-// check if the input value is a number
-// if(!isNaN(inputValue)){
-// check if the input value is less than 18
-// if(inputValue<18){
-// display error message
-//     document.querySelector('.error').innerHTML="You are not allowed to enter";
-// }
-// else{       
-
-// display success message
-
-//             document.querySelector('.error').innerHTML="You are allowed to enter";
-//         }
-
-//     }
-//     else{
-//         alert("Please enter a number");
-//     }
-
-
-// });
-
-
-
-// ternary operator 
-
-var age = 19;
-var canDrive;
-// if (age>16){
-//     canDrive='yes';
-// } 
-
-//  else {
-//     canDrive='no';
-// }
-
-// above code using ternary operator
-
-// canDrive=age>16?'yes':'no';
-// console.log(canDrive);
-// ternary op use to set dfault value of parameter
-let querySelect = document.querySelector('#query-select');
-let ansSelect = document.querySelector('#answer-select');
-let selectQuantityElement = document.querySelector('#physical_quantity');
 selectQuantityElement.addEventListener("change", function (event) {
-    // check previous value of select element
-    // let previousValue=event.target.value;
-    // make it empty    
-    // find index of selected option
-    // let selectedIndex=selectQuantityElement.selectedIndex;
-    // console.log(selectedIndex);
     selectedQuantityValue = event.target.value;
-    console.log(selectedQuantityValue);
+    // console.log(selectedQuantityValue);
     switch (selectedQuantityValue) {
         case 'numberSystem':
-            handleChildSelectPhysicalEntity(numberSystemObject, querySelect,ansSelect);
-            
-
-
-            // let numberSystemObject = {
-            //     'bin': 'Binary',
-            //     'ter': 'Ternary',
-            //     'qua': 'Quaternary',
-            //     'qui': 'Quinary',
-            //     'sen': 'Senary',
-            //     'sep': 'Septenary',
-            //     'oct': 'Octal',
-            //     'non': 'Nonary',
-            //     'dec': 'Decimal',
-            //     'und': 'Undecimal',
-            //     'duo': 'Duodecimal',
-            //     'hex': 'Hexadecimal',
-            //     'vig': 'Vigesimal',
-            //     'sex': 'Sexagesimal',
-            // };
-
-            // if (querySelect.hasChildNodes()) {
-            //     // remove all child nodes
-            //     querySelect.innerHTML = '';
-            //     ansSelect.innerHTML = '';
-            //     // loop through the object
-            //     for (let key in numberSystemObject) {
-            //         // add option to the select element 
-            //         addOption(querySelect, key, numberSystemObject[key]);
-            //         addOption(ansSelect, key, numberSystemObject[key]);
-            //     }
-
-
-            // }
-
-
+            handleChildSelectPhysicalEntity(numberSystemObject, querySelect, ansSelect);
             break;
         case 'length':
-            handleChildSelectPhysicalEntity(lengthObject, querySelect,ansSelect);
-
-            // // alert("length");
-            // console.log("you have selected length");
-            // add child to the select element in another select tag
-            // object to hold all values of length
-            // let lengthObject = {
-            //     'mm': 'Millimeter',
-            //     'cm': 'Centimeter',
-            //     'm': 'Meter',
-            //     'km': 'Kilometer',
-            //     'in': 'Inch',
-            //     'ft': 'Feet',
-            //     'yd': 'Yard',
-            //     'mi': 'Mile'
-            // };
-            // check select option empty or not 
-            // if (querySelect.hasChildNodes()) {
-            //     // remove all child nodes
-            //     querySelect.innerHTML = '';
-            //     ansSelect.innerHTML = '';
-            //     // loop through the object
-            //     for (let key in lengthObject) {
-            //         // add option to the select element 
-            //         addOption(querySelect, key, lengthObject[key]);
-            //         addOption(ansSelect, key, lengthObject[key]);
-            //     }
-
-            // }
-
-
+            handleChildSelectPhysicalEntity(lengthObject, querySelect, ansSelect);
             break;
         case 'speed':
-            handleChildSelectPhysicalEntity(speedObject, querySelect,ansSelect);
-
-            // let speedObject = {
-            //     'mph': 'Miles per hour',
-            //     'kph': 'Kilometers per hour',
-            //     'fps': 'Feet per second',
-            //     'knot': 'Knot'
-            // };
-            // if (querySelect.hasChildNodes()) {
-            //     querySelect.innerHTML = '';
-            //     ansSelect.innerHTML = '';
-            //     for (let key in speedObject) {
-            //         addOption(querySelect, key, speedObject[key]);
-            //         addOption(ansSelect, key, speedObject[key]);
-            //     }
-
-            // }
-
-
-
+            handleChildSelectPhysicalEntity(speedObject, querySelect, ansSelect);
             break;
         case 'mass':
-            handleChildSelectPhysicalEntity(massObject, querySelect,ansSelect);
-
-            // let massObject = {
-
-            //     'mg': 'Milligram',
-            //     'g': 'Gram',
-            //     'kg': 'Kilogram',
-            //     't': 'Ton',
-            //     'oz': 'Ounce',
-            //     'lb': 'Pound',
-            //     'st': 'Stone'
-            // };
-            // if (querySelect.hasChildNodes()) {
-            //     querySelect.innerHTML = '';
-            //     ansSelect.innerHTML = '';
-            //     for (let key in massObject) {
-            //         addOption(querySelect, key, massObject[key]);
-            //         addOption(ansSelect, key, massObject[key]);
-            //     }
-            // }
-
-
+            handleChildSelectPhysicalEntity(massObject, querySelect, ansSelect);
             break;
         case 'digitalStore':
-            handleChildSelectPhysicalEntity(digitalStoreObject, querySelect,ansSelect);
-
-            // let digitalStoreObject = {
-            //     'b': 'Byte',
-            //     'kb': 'Kilobyte',
-            //     'mb': 'Megabyte',
-            //     'gb': 'Gigabyte',
-            //     'tb': 'Terabyte',
-            //     'pb': 'Petabyte'
-            // };
-            // if (querySelect.hasChildNodes()) {
-            //     querySelect.innerHTML = '';
-            //     ansSelect.innerHTML = '';
-            //     for (let key in digitalStoreObject) {
-            //         addOption(querySelect, key, digitalStoreObject[key]);
-            //         addOption(ansSelect, key, digitalStoreObject[key]);
-            //     }
-            // }
-
-
+            handleChildSelectPhysicalEntity(digitalStoreObject, querySelect, ansSelect);
             break;
-
         default:
             break;
     }
-
-
-
 });
+
 function addOption(selectElement, optionValue, optionText) {
     // create option element    
     let optionElement = document.createElement('option');
@@ -298,8 +109,6 @@ function addOption(selectElement, optionValue, optionText) {
     // append option element to select element
     selectElement.appendChild(optionElement);
 }
-
-
 
 // create a function to get user input from input tag in js
 function getInput() {
@@ -320,12 +129,193 @@ document.querySelector('input').addEventListener("input", function (event) {
     var inputValue = document.querySelector('input').value;
     // check if the input value is a number
     if (!isNaN(inputValue)) {
-        // console.log(inputValue);
-
+        console.log(inputValue);
+        console.log(querySelect.value);
+        console.log(ansSelect.value);
+        changeOneUnitToOther(inputValue, querySelect.value, ansSelect.value);
     }
-
 }
 );
 
+function convert(value, supplyUnit, requiredUnit) {
+
+    var result = 0;
+    switch (requiredUnit) {
+        case 'mg':
+            switch (supplyUnit) {
+                case 'g':
+                    result = value * 1000;
+                    break;
+                case 't':
+                    result = (value * 1000000000).toExponential(4);
+                    break;
+                case 'oz':
+                    result = (value * 28350);
+                    break;
+                case 'lb':
+                    result = (value * 453592).toExponential(4);;
+                    break;
+                case 'st':
+                    result = (value * 6350000) * toExponential(4);
+                    break;
+                case 'kg':
+                    result = (value * 1000000).toExponential(4);
+
+                    break;
+                default:
+                    break;
+            }
+            break;
+
+        case 'g':
+            switch (supplyUnit) {
+                case 'mg':
+                    result = value / 1000;
+                    // console.log(result);
+                    break;
+                case 'kg':
+                    result = value * 1000;
+                    break;
+                case 't':
+                    result = (value * 1000000).toExponential();
+                    break;
+                case 'oz':
+                    result = value * 28.35;
+                    break;
+                case 'lb':
+                    result = value * 454;
+                    break;
+                case 'st':
+                    result = value * 6350;
+                    break;
+
+                default:
+                    break;
+            }
+            break;
+        case 'kg':
+            switch (supplyUnit) {
+                case 'mg':
+                    result = (value / 1000000).toExponential();
+                    // alert(result);
+                    break;
+                case 'g':
+                    result = value / 1000;
+                    alert(result);
+                    break;
+                case 't':
+                    result = value * 1000;
+                    break;
+                case 'oz':
+                    result = value / 35.274;
+                    break;
+                case 'lb':
+                    result = (value / 2.205).toExponential();
+                    break;
+                case 'st':
+                    result = (value * 6.35).toExponential(4);
+                    break;
+                default:
+                    break;
+            }
+            break;
+
+        case 't':
+            switch (supplyUnit) {
+                case 'mg':
+                    result = value / 1000000000;
+                    alert(result);
+                    console.log(result);
+                    break;
+                case 'g':
+                    break;
+                case 'kg':
+                    break;
+                case 'oz':
+                    break;
+                case 'lb':
+                    break;
+                case 'st':
+                    break;
+                default:
+                    break;
+            }
+            break;
+        case 'oz':
+            switch (supplyUnit) {
+                case 'mg':
+                    result = (value / 28350).toExponential(4);
+                    alert(result);
+                    break;
+                case 'g':
+                    break;
+                case 'kg':
+                    break;
+                case 't':
+                case 'lb':
+                    break;
+                case 'st':
+                    break;
+                default:
+                    break;
+            }
+            break;
+        case 'lb':
+            switch (supplyUnit) {
+                case 'mg':
+                    result = (value / 453592).toExponential(4);
+                    break;
+                case 'g':
+                    break;
+                case 'kg':
+                    break;
+                case 'oz':
+                    break;
+                case 't':
+                    break;
+                case 'st':
+                    break;
+                default:
+                    break;
+            }
+            break;
+        case 'st':
+            switch (supplyUnit) {
+                case 'mg':
+                    result = (value / 6350000).toExponential(4);
+                    // alert(result);
+                    break;
+                case 'g':
+                    result = value * 6350;
+                    break;
+                case 'kg':
+                    result = value * 6.350;
+
+                    break;
+                case 'oz':
+                    result = value * 224;
+
+                    break;
+                case 'lb':
+                    result = value * 14;
+
+                    break;
+
+                default:
+                    break;
+            }
+            break;
+        default:
+            break;
+    }
+    return result;
+}
 
 
+
+function changeOneUnitToOther(value, supplyUnit, otherUnit) {
+    let result = 0;
+    result = convert(value, supplyUnit, otherUnit);
+    // add this result to right answer input box
+    document.querySelector('#answer').value = result;
+}

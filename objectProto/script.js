03d2c4ea18a9c06a90d5e48518ddf1469a1ea4eb
pvm-77 <sfzalig@gmail.com>
2022-01-28 15:17:14 +0530

    object proto folder added

---
+++ objectProto/script.js
@@ -0,0 +1,13 @@
+let unit={
+    unitId:"101",
+    unitName:"Unit 1",
+    subjectName:"Physics",
+    subjectCode:"PHY101",
+}
+console.log(unit);
+
+let chapter={
+    chapterId:"101",
+    chapterName:"Chapter 1",  
+}
+console.log(chapter);
